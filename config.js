34cd802a02612d31eeaf3c539842be9a9a4419ff
Rick Gallegos <duulketariakan@yahoo.com>
2018-03-29 20:34:44 -0700

    Add Eslint loader

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const HEROKU_URL = JSON.stringify('https://localhost:5000');
+//TODO: Add a heroku link to line 5
 
 if (process.env.NODE_ENV === 'production') {
   HEROKU_URL = JSON.stringify('https://INSERT-HEROKU-LINK.herokuapp.com');
