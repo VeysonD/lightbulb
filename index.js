c94704ee3661488308168609edce2c5a7c8d80e3
Rick Gallegos <duulketariakan@yahoo.com>
2018-03-29 19:51:58 -0700

    Set up initial node server

---
+++ index.js
@@ -0,0 +1,8 @@
+import app from './server/request-handler';
+
+const PORT = process.env.PORT || 5000;
+const SERVER = process.env.SERVER || 'https://127.0.0.1';
+
+const server = app.listen(PORT);
+
+console.log(`Server is listening at ${SERVER}:${PORT}`);
