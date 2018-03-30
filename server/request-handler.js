3d4eeb8552529c340aa904a83a3891ebb6eb3af5
Rick Gallegos <duulketariakan@yahoo.com>
2018-03-29 21:45:57 -0700

    Prepare for deployment

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(express.static('public'));
+app.use(morgan('dev'));
 
 app.get('/', (req, res, next) => {
   console.log('Testing get');
