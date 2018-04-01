d27a289964b66e809f072b4a1974fbb7e9f41ba5
Rick Gallegos <duulketariakan@yahoo.com>
2018-03-31 23:23:13 -0700

    Add body parser

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import bodyParser from 'body-parser';
 
 import router from './api/api-handler';
 
@@ -7,6 +8,8 @@ const app = express();
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   console.log('Testing get');
