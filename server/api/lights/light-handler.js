5249dcfebeb33ad170c0fb8cd3487fd17b33d6ee
Rick Gallegos <duulketariakan@yahoo.com>
2018-04-01 00:24:33 -0700

    Add a middleware to check if light is connected to the wifi

--- server/api/lights/light-handler.js
+++ server/api/lights/light-handler.js
@@ -3,6 +3,7 @@ import db from './../../../db/db-config';
 
 const lightRouter = express.Router();
 
+
 lightRouter.get('/all', (req, res) => {
   db.light
     .findAll()
@@ -24,9 +25,64 @@ lightRouter.get('/:id', (req, res) => {
     });
 });
 
+lightRouter.post('/:id/*', (req, res, next) => {
+  const { id } = req.params;
+  db.sequelize
+    .query(`SELECT connected_wifi FROM lights WHERE id = ${id}`)
+    .then((connectionStatus) => {
+      if (connectionStatus[0].length === 0 || !connectionStatus[0][0].connected_wifi) {
+        res.send('This light is not connected to the wifi');
+      } else {
+        req.locals = { id };
+        next();
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send('There was an error when checking the lights');
+    });
+});
+
+lightRouter.post('/:id/color', (req, res) => {
+  const { id } = req.locals;
+  const { color } = req.body;
+  db.light
+    .update({
+      color,
+    }, {
+      where: {
+        id,
+      },
+      returning: true,
+    })
+    .then((light) => {
+      if (light[1].length === 0) {
+        res.send('That light does not exist');
+      } else {
+        const { name } = light[1][0].dataValues;
+        db.changelog
+          .create({
+            log: `${name}'s color changed to ${color}`,
+            lightId: id,
+          })
+          .then((changelog) => {
+            res.send(changelog);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.send('There was an error when inserting a new log');
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send('There was an error when updating the color');
+    });
+});
+
 
 lightRouter.post('/:id/switch', (req, res) => {
-  const { id } = req.params;
+  const { id } = req.locals;
   db.sequelize
     .query(`UPDATE lights SET switched_on = NOT switched_on WHERE id = ${id} RETURNING name, switched_on`)
     .spread((light) => {
@@ -53,7 +109,7 @@ lightRouter.post('/:id/switch', (req, res) => {
     })
     .catch((error) => {
       console.error(error);
-      res.send('There was an error with your request');
+      res.send('There was an error when switching the light');
     });
 });
 
