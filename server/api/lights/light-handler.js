ffcb1b201f33e66deaa0505f55ed38e6ab845076
Rick Gallegos <duulketariakan@yahoo.com>
2018-04-01 19:01:36 -0700

    Fully working WIFI authentication for the light device

--- server/api/lights/light-handler.js
+++ server/api/lights/light-handler.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bcrypt from 'bcrypt'; // MOVE TO AUTH
 
 import db from './../../../db/db-config';
 import { handleAuth } from './auth/light-auth-settings';
@@ -31,26 +32,55 @@ lightRouter.get('/:id', (req, res) => {
 
 lightRouter.post('/:id/*', (req, res, next) => {
   const { id } = req.params;
-  const { wifi } = req.body;
   db.sequelize
-    .query(`SELECT connected_wifi FROM lights WHERE id = ${id}`)
+    .query(`SELECT connected_wifi, wifi_id, wifi_pass FROM lights WHERE id = ${id}`)
     .then((connectionStatus) => {
-      if (connectionStatus[0].length === 0) {
+      console.log('What is the connectionStatus: ', connectionStatus);
+      const connect = connectionStatus[0];
+      if (connect.length === 0) {
         res.send('This light is not associated with any wifi');
-      } else if (!connectionStatus[0][0].connected_wifi) { // not connected
-        req.locals = { id, wifi };
-        // if it isn't connected to wifi
-        // check if the wifi is given from line 30;
-          // if it isn't then res.send and error out res.send(please provide wifi to connect to)
-          //if it is then check if the wifi corresponds to a wifi in the DB
-            // if the wifi is in the database then update the wifi_id and update connected_wifi to true
-              // update changelog to show that light connceted to wifi_id
-                //next() (??? or res.send???)
-            // if the wifi isn't in the database then res.send and error out res.send(please use a valid wifi)
-        next();
-      } else { // connected
-        req.locals = { id, wifi }; // maybe don't need to send wifi
-        next();
+      } else {
+        const connectedWifi = connect[0].connected_wifi;
+        const wifiId = connect[0].wifi_id;
+        const wifiPass = connect[0].wifi_pass;
+        if (connectedWifi) { // it is connected to a wifi
+          req.locals = { id };
+          next();
+        } else if (!wifiId) {
+          res.send('This light is not associated with a wifi');
+        } else {
+          db.sequelize
+            .query(`SELECT password FROM wifis where id=${wifiId}`)
+            .then((passResult) => {
+              const wifiTruePass = passResult[0][0].password;
+              console.log('PASSWORDS: ', wifiTruePass, wifiPass);
+              if (wifiTruePass === wifiPass) {
+                db.light
+                  .update({
+                    connected_wifi: true,
+                  }, {
+                    where: {
+                      id,
+                    },
+                  })
+                  .then((connectionUpdate) => {
+                    console.log('What is the connection update: ', connectionUpdate);
+                    req.locals = { id };
+                    next();
+                  })
+                  .catch((error) => {
+                    console.error(error);
+                    res.send('There was an error while updating the connection');
+                  });
+              } else {
+                res.send('Light password for wifi is incorrect');
+              }
+            })
+            .catch((error) => {
+              console.error(error);
+              res.send('There was an error while retrieving the wifi password');
+            });
+        }
       }
     })
     .catch((error) => {
