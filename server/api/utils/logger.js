d28ba04c60428b594d94121c03dbfcd6217102dd
Rick Gallegos <duulketariakan@yahoo.com>
2018-04-04 00:41:31 -0700

    Update logger to be more general

--- server/api/utils/logger.js
+++ server/api/utils/logger.js
@@ -1,21 +1,16 @@
 import db from './../../../db/db-config';
 
-const addLog = (logMessage, item, id, req, res, next) => {
+const addLog = (logMessage, item, id) => {
   const entry = {};
   entry[item] = id;
   entry.log = logMessage;
   db.changelog
     .create(entry)
     .then((changelog) => {
-      if (next) {
-        next();
-      } else {
-        res.send(changelog);
-      }
+      console.log('Changelog: ', changelog);
     })
     .catch((error) => {
       console.error(error);
-      res.send('There was an error when inserting a new log');
     });
 };
 
