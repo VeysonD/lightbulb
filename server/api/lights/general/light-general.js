09477f3f03d37cc244a2837e64ab16888243ec26
Rick Gallegos <duulketariakan@yahoo.com>
2018-04-07 01:55:12 -0700

    All API routes have proper error handling and separated the control functions

--- server/api/lights/general/light-general.js
+++ server/api/lights/general/light-general.js
@@ -1,6 +1,9 @@
-import bcrypt from 'bcrypt';
-import db from './../../../../db/db-config';
-import addLog from './../../../utils/logger';
+import {
+  newLightCtrl,
+  retrieveAllCtrl,
+  retrieveLogsCtrl,
+  retrieveOneCtrl,
+} from './../../../controllers/lights/light-general-ctrl';
 
 const newLight = (req, res) => {
   const {
@@ -16,105 +19,57 @@ const newLight = (req, res) => {
     wifi,
     wifiPass,
   } = req.body;
-  db.wifi
-    .findAll({
-      attributes: [
-        'id',
-        'password',
-      ],
-      where: {
-        ssid: wifi,
-      },
-    })
-    .then((data) => {
-      if (data.length === 0) {
-        res.send('There is no wifi by that name');
-      } else {
-        const wifiHash = data[0].dataValues.password;
-        const wifiId = data[0].dataValues.id;
-        bcrypt.compare(wifiPass, wifiHash, (err, check) => {
-          if (check) {
-            const connectedWifi = true;
-            db.light
-              .create({
-                charging,
-                color,
-                dim,
-                ip,
-                latitude,
-                longitude,
-                location,
-                name,
-                switched_on: switchedOn,
-                connected_wifi: connectedWifi,
-                wifi_id: wifiId,
-                wifi_pass: wifiPass,
-              })
-              .then((light) => {
-                const { id } = light.dataValues;
-                const log = `${color} light was added to ${wifi} network`;
-                addLog(log, 'lightId', id);
-                res.send(`New light named ${name} was created`);
-              })
-              .catch((error) => {
-                console.error(error);
-                res.send('There was an error when entering the new light data');
-              });
-          } else {
-            res.send('Wifi password is wrong');
-          }
-        });
-      }
+
+  newLightCtrl(
+    charging, color,
+    dim, ip,
+    latitude, longitude, location,
+    name, switchedOn,
+    wifi, wifiPass,
+  )
+    .then((log) => {
+      res.send(log);
     })
     .catch((error) => {
       console.error(error);
-      res.send('There was an error when checking the wifi');
+      res.send(`${error.name}: ${error.message}`);
     });
 };
 
 const retrieveAll = (req, res) => {
-  db.light
-    .findAll()
+  retrieveAllCtrl
     .then((lights) => {
       res.send(lights);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send(`${error.name}: ${error.message}`);
     });
 };
 
 const retrieveLogs = (req, res) => {
   const { id } = req.params;
-  db.changelog
-    .findAll({
-      where: {
-        lightId: id,
-      },
-    })
-    .then((data) => {
-      if (data.length === 0) {
-        res.send('There are no logs for this device');
-      } else {
-        const logs = data.map((log) => {
-          const logData = log.dataValues;
-          return [logData.log, logData.createdAt];
-        });
-        res.send(logs);
-      }
+
+  retrieveLogsCtrl(id)
+    .then((logs) => {
+      res.send(logs);
     })
     .catch((error) => {
       console.error(error);
-      res.send('There was an error when trying to find the logs');
+      res.send(`${error.name}: ${error.message}`);
     });
 };
 
 const retrieveOne = (req, res) => {
   const { id } = req.params;
-  db.sequelize
-    .query(`SELECT * FROM lights WHERE id = ${id}`)
+
+  retrieveOneCtrl(id)
     .then((light) => {
-      res.send(light[0]);
+      res.send(light);
     })
     .catch((error) => {
       console.error(error);
-      res.send('There was an error with your light request');
+      res.send(`${error.name}: ${error.message}`);
     });
 };
 
---
+++ server/controllers/lights/light-general-ctrl.js
@@ -0,0 +1,144 @@
+import db from './../../../db/db-config';
+import { comparePass } from './../../utils/password-check';
+import addLog from './../../utils/logger';
+
+
+const createNewLight = (
+  charging, color,
+  dim, ip,
+  latitude, longitude, location,
+  name, switchedOn,
+  wifi, wifiId, wifiPass,
+) =>
+  new Promise((resolve, reject) => {
+    const connectedWifi = true;
+    db.light
+      .create({
+        charging,
+        color,
+        dim,
+        ip,
+        latitude,
+        longitude,
+        location,
+        name,
+        switched_on: switchedOn,
+        connected_wifi: connectedWifi,
+        wifi_id: wifiId,
+        wifi_pass: wifiPass,
+      })
+      .then((light) => {
+        const { id } = light.dataValues; // TODO: CHECK ID
+        const log = `${color} light was added to ${wifi} network`;
+        addLog(log, 'lightId', id);
+        resolve(log);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+
+const newLightCtrl = (
+  charging, color,
+  dim, ip,
+  latitude, longitude, location,
+  name, switchedOn,
+  wifi, wifiPass,
+) =>
+  new Promise((resolve, reject) => {
+    db.wifi
+      .findAll({
+        attributes: [
+          'id',
+          'password',
+        ],
+        where: {
+          ssid: wifi,
+        },
+      })
+      .then((data) => {
+        if (data.length === 0) {
+          reject(new Error('There is no wifi by that name'));
+        } else {
+          const wifiHash = data[0].dataValues.password;
+          const wifiId = data[0].dataValues.id;
+
+          comparePass(wifiPass, wifiHash)
+            .then((check) => {
+              if (check) {
+                createNewLight(
+                  charging, color,
+                  dim, ip,
+                  latitude, longitude, location,
+                  name, switchedOn,
+                  wifi, wifiId, wifiPass,
+                )
+                  .then((log) => {
+                    resolve(log);
+                  })
+                  .catch((error) => {
+                    reject(error);
+                  });
+              } else {
+                reject(new Error('The wifi password provided is incorrect'));
+              }
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        }
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+
+const retrieveAllCtrl = () =>
+  new Promise((resolve, reject) => {
+    db.light
+      .findAll()
+      .then((lights) => {
+        resolve(lights);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+
+const retrieveLogsCtrl = id =>
+  new Promise((resolve, reject) => {
+    db.changelog
+      .findAll({
+        where: {
+          lightId: id,
+        },
+      })
+      .then((data) => {
+        if (data.length === 0) {
+          reject(new Error('There are no logs for this device'));
+        } else {
+          const logs = data.map((log) => {
+            const logData = log.dataValues;
+            return [logData.log, logData.createdAt];
+          });
+          resolve(logs);
+        }
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+
+const retrieveOneCtrl = id =>
+  new Promise((resolve, reject) => {
+    db.sequelize
+      .query(`SELECT * FROM lights WHERE id = ${id}`)
+      .then((light) => {
+        resolve(light[0]);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+
+export { newLightCtrl, retrieveAllCtrl, retrieveLogsCtrl, retrieveOneCtrl };
